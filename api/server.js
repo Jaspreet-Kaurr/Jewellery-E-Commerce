f151758c6b2de068ed850d50bb9505e10290aa9a
Jaspreet kaur <jaskaur.pb65@gmail.com>
2025-11-29 13:41:47 +0530

    Removed vercel setup for backend

--- api/server.js
+++
@@ -1,7 +0,0 @@
-import serverless from "serverless-http";
-import app from "../server/src/server.js";
-
-const handler = serverless(app);
-
-// Vercel needs default export
-export default handler;
--- server/src/server.js
+++ server/src/server.js
@@ -11,7 +11,6 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-// app.use(cors());
 app.use(cors());
 app.use(express.json());
 
@@ -26,7 +25,7 @@ app.use("/api", paymentRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
-export default app;
+// export default app;
